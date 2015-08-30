f9d0bd86e68793316e65357e8d0aad3df8366cd5
admin <admin@admins-iMac.local>
2015-08-31 05:01:45 +0700

    Add Cosmos library

---
+++ RottenTomatoes/GlobalConfig.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalConfig.swift
+//  RottenTomatoes
+//
+//  Created by admin on 8/31/15.
+//  Copyright (c) 2015 hoang. All rights reserved.
+//
+
+import Foundation
+class GlobalConfig{
+    static let BOX_OFFICE_API = "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json"
+    static let TOP_DVDS_API = "https://gist.githubusercontent.com/timothy1ee/e41513a57049e21bc6cf/raw/b490e79be2d21818f28614ec933d5d8f467f0a66/gistfile1.json"
+}
