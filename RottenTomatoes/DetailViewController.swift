89210c3dd1eaef5bc5551ac1e4589c3b19b5824c
admin <admin@admins-iMac.local>
2015-08-31 23:21:44 +0700

    Finish UI

---
+++ RottenTomatoes/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  RottenTomatoes
+//
+//  Created by admin on 8/31/15.
+//  Copyright (c) 2015 hoang. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var item:NSDictionary!
+
+    @IBOutlet weak var synopys: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var poster: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
